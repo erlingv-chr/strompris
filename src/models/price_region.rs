61bbf8497733fb2385aa1fd6bede4d3ddf909448
Erling <81021982+erlingv-chr@users.noreply.github.com>
2024-07-19 21:33:20 +0200

    Derived traits

--- src/models/price_region.rs
+++ src/models/price_region.rs
@@ -1,6 +1,8 @@
+use derive_more::Display;
+
 /// Because the different regions of Norway has different access to power, Norway is
 /// divided into 5 price regions. Each of these regions has its own hourly price.
-
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Display)]
 pub enum PriceRegion {
     /// Oslo / Øst-Norge
     NO1,
